08880aefef0803e2ae969bca31a7b28d3687d0e7
NMHO <nicklasmholesen@gmail.com>
2016-10-28 10:54:47 +0200

    Ændret Sanscastle layout

--- BlodtryksApplikation/BlodtryksApplikationLogikLag/NulpunktsjusteringLL.cs
+++ BlodtryksApplikation/BlodtryksApplikationLogikLag/NulpunktsjusteringLL.cs
@@ -8,14 +8,15 @@ using BlodtryksApplikationDataLag;
 namespace BlodtryksApplikationLogikLag
 {
     /// <summary>
-    /// 
+    /// Klasse der henter den indlæste nulpunktsspændings fra datalaget
     /// </summary>
     public class NulpunktsjusteringLL
     {
         private double NulpunktsVærdi;
         private NulpunktsjusteringDL NPJDL;
+
         /// <summary>
-        /// 
+        /// Constructor
         /// </summary>
         public NulpunktsjusteringLL()
         {       
@@ -23,9 +24,11 @@ namespace BlodtryksApplikationLogikLag
             NulpunktsVærdi = 0;
         }
         /// <summary>
-        /// 
+        /// Henter den indlæste nulpunktsspændings fra datalaget
         /// </summary>
-        /// <returns></returns>
+        /// <returns>
+        /// Returnerer den indlæste nulpunktsspændings
+        /// </returns>
         public double hentNulpunktsSpænding()
         {
             NulpunktsVærdi = NPJDL.indlæsNulpunktsSpænding();
