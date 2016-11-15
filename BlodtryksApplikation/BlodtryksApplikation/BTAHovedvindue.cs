c60749a33e63fd5ea215a30ab17c3a0bc00d70d4
NMHO <nicklasmholesen@gmail.com>
2016-11-15 12:28:58 +0100

    Revert "rettet tekstboks"
    This reverts commit 729d9d633523e300b641136901130e11a9fda6be.

--- BlodtryksApplikation/BlodtryksApplikation/BTAHovedvindue.cs
+++ BlodtryksApplikation/BlodtryksApplikation/BTAHovedvindue.cs
@@ -73,7 +73,7 @@ namespace BlodtryksApplikation
         private void btnNulpunktsjusterSystem_Click(object sender, EventArgs e)
         {
             NulpunktsVærdi = NPJLL.hentNulpunktsSpænding();
-            MessageBox.Show("Nulpunktsjustering er foretaget.");
+            MessageBox.Show("Nuljustering er foretaget.");
         }
 
         private void btnToolStripNulpunktsjusterSystem_Click(object sender, EventArgs e)
