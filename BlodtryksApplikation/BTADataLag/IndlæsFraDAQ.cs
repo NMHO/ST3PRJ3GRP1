dac18e37fda1563deef694d4c56833f66575fd76
NMHO <nicklasmholesen@gmail.com>
2016-11-23 16:08:12 +0100

    ændret DAQ indlæsning

--- BlodtryksApplikation/BTADataLag/IndlæsFraDAQ.cs
+++ BlodtryksApplikation/BTADataLag/IndlæsFraDAQ.cs
@@ -7,6 +7,7 @@ using NationalInstruments;
 using NationalInstruments.DAQmx;
 using Interfaces;
 
+
 namespace BTADataLag
 {    
 
@@ -16,7 +17,62 @@ namespace BTADataLag
     public abstract class IndlæsFraDAQ : IReadInput
     {
 
+        //private NI_DAQVoltage datacollector;      
 
+        
+        private NationalInstruments.DAQmx.Task analogInTask;
+        private AnalogSingleChannelReader reader;
+        private int samplesPerChannel = 100;
+
+
+
+        public IndlæsFraDAQ()
+        {
+            /*
+            datacollector = new NI_DAQVoltage();
+            datacollector.sampleRateInHz = 1000;
+            datacollector.deviceName = "Dev1/ai0";
+            datacollector.samplesPerChannel = 100;*/
+
+
+            analogInTask = new NationalInstruments.DAQmx.Task();
+
+            analogInTask.AIChannels.CreateVoltageChannel("Dev1/ai0", "myAIChannel", AITerminalConfiguration.Differential, 0, 5, AIVoltageUnits.Volts);
+
+            analogInTask.Timing.ConfigureSampleClock("", 1000, SampleClockActiveEdge.Rising, SampleQuantityMode.FiniteSamples, samplesPerChannel);
+
+            reader = new AnalogSingleChannelReader(analogInTask.Stream);
+
+        }
+
+
+        /// <summary>
+        /// Indlæser en serie af samples fra NI-DAQ i volt
+        /// </summary>
+        /// <param name="samples">Antal ønsket datapunkter</param>
+        /// <returns>Returnerer en liste af datapunkter</returns>  
+        public List<double> ReadInput(int samples)
+        {
+            
+            var seqList = new List<double>(reader.ReadMultiSample(samples));
+
+            //analogInTask.Dispose();
+
+            return seqList;
+
+            //return null;
+
+            //return testUdenDAQ();
+
+            //datacollector.samplesPerChannel = samples;     
+
+            //datacollector.getVoltageSeqBlocking();
+
+            //return datacollector.currentVoltageSeq;
+        }
+
+
+        /*
         static Random rnd = new Random();
         private List<double> testUdenDAQ()
         {
@@ -27,31 +83,6 @@ namespace BTADataLag
                 lst.Add(rnd.Next(0, 5));
             }
             return lst;
-        }
-        
-
-        /// <summary>
-        /// Indlæser en serie af samples fra NI-DAQ i volt
-        /// </summary>
-        /// <param name="samples">Antal ønsket datapunkter</param>
-        /// <returns>Returnerer en liste af datapunkter</returns>  
-        public List<double> ReadInput(int samples)
-        {
-
-            NationalInstruments.DAQmx.Task analogInTask = new NationalInstruments.DAQmx.Task();
-            analogInTask.Timing.ConfigureSampleClock("", 1000, SampleClockActiveEdge.Rising, SampleQuantityMode.FiniteSamples);
-            AIChannel myAIChannel;
-            myAIChannel = analogInTask.AIChannels.CreateVoltageChannel("Dev1/ai0", "myAIChannel",
-                AITerminalConfiguration.Differential, 0, 5, AIVoltageUnits.Volts);
-
-            AnalogSingleChannelReader reader = new AnalogSingleChannelReader(analogInTask.Stream);
-
-            var seqList = new List<double>(reader.ReadMultiSample(samples));
-
-            return seqList;
-
-
-            //return testUdenDAQ();
-        }
+        }*/
     }
 }
